61edd5e5b7eaddbaa5042b5cffe3a92acd8713e6
dimohan <13104196063@163.com>
2019-06-22 16:19:21 +0800

    用户登录发送验证码，请求获取初始化验证码的参数

--- src/App.vue
+++ src/App.vue
@@ -12,5 +12,8 @@ export default {
 }
 </script>
 <style lang="less">
+  #app {
+    height: 100%
+  }
 
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-// import ElementUI from 'element-ui';
-// import 'element-ui/lib/theme-chalk/index.css';
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import router from './router'
-// Vue.use(ElementUI);
+import './styles/index.less'
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
